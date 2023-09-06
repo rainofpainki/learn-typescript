577350cc0436338ecf97ed2efa53d31d32c0fa95
Sunha Ryu <rainofpainki@naver.com>
2023-09-06 11:15:10 +0900

    addTwoTodoItems() 함수 구현

--- quiz/1_todo/src/index.ts
+++ quiz/1_todo/src/index.ts
@@ -41,9 +41,9 @@ function showCompleted(): { id: number; title: string; done: boolean }[] {
   return todoItems.filter(item => item.done);
 }
 
-// TODO: 아래 함수의 내용을 채워보세요. 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수입니다.
-function addTwoTodoItems() {
-  // addTodo() 함수를 두 번 호출하여 todoItems에 새 할 일이 2개 추가되어야 합니다.
+function addTwoTodoItems(): void {
+  addTodo({ id: 4, title: '타입 정의', done: false });
+  addTodo({ id: 5, title: '복습', done: false });
 }
 
 // NOTE: 유틸 함수
